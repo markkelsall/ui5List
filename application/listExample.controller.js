fdda266f4c44dc5796ec4692b1ce347e63bd3f14
markkelsall <mark.kelsall@gmail.com>
2015-08-18 09:17:28 +0100

    Update for the code and adding dos and dont's

--- application/listExample.controller.js
+++ application/listExample.controller.js
@@ -3,10 +3,13 @@ sap.ui.controller("application.listExample", {
 	onInit: function() {
 		this.getView().setDisplayBlock(true);
 		responsiveExampleController = this;
+
+		var jExampleList = new sap.ui.model.json.JSONModel(exampleList);
+		sap.ui.getCore().setModel(jExampleList, "exampleList");
   },
 
 	onItemPress : function (evt) {
-		var selectedItem = evt.getSource().getBindingContext("exampleList").getObject();
+		var selectedItem = evt.getSource().getSelectedItem().getBindingContext("exampleList").getObject();
 		console.log("selected: " + selectedItem.title);
 	}
 });
